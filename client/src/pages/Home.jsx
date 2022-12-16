ccd4b8911919158ea4688aa72b9dbbd318421b11
Gabriel Borges <gabriel.moura2009@hotmail.com>
2022-12-15 22:14:31 -0300

    feat: add initial files react

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import HelloWorld from "../components/HelloWorld";
+
+const Home = () => {
+  return (
+    <>
+      <HelloWorld />
+      <h1>O Warley Bolsonarista que faz o L e vota no <span style={{ background: 'red'}}>13</span></h1>
+    </>
+  )
+}
+
+export default Home
